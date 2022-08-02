a0dcf6e6adfd317dd88268f8c6a6f483015ac831
nano0144 <fer_0144@hotmail.com>
2022-08-02 13:18:42 -0300

    App config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { getDataBase } from './application/api';
+import { useState, useEffect, useRef } from 'react';
+import SearchSelect from './components/SearchSelect';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (users.length === 0) {
+      getData();
+    }
+  }, [users])
+
+  const getData = async () => {
+    const resul = await getDataBase();
+    setUsers(resul.docs);
+  }
+
+  //--------------------------------------------------------------
+  const myRef = useRef(null)
+  const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+  const executeScroll = () => scrollToRef(myRef)
+
+
+  // https://firebase.google.com/docs/firestore/query-data/get-data?hl=es-419
+  // https://console.firebase.google.com/u/1/project/my-first-project-9cde2/firestore/data/~2Fusers~2Fs570dXEBHQxJCeb63xgF
+
   return (
-    <div className="App">
-    
+    <div className="App" ref={myRef}>
+      <div className="App-header">
+        <h1>Nano producciones!</h1>
+        <div>
+          <button onClick={getData}>Recargar</button>
+          <p></p>
+        </div>
+        <SearchSelect users={users} getData={getData} />
+        <div>
+          <button onClick={executeScroll}> Volver al inicio </button>
+        </div>
+      </div>
     </div>
   );
 }
